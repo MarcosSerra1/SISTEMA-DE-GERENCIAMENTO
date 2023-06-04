b5def853c9ad8717eafa74fb86daa1fe07571389
Marcos Serra <marcosserra133@gmail.com>
2023-06-03 21:12:09 -0300

    update cadastrofuncionario.py
    Correção caminho do cadastro.ui

--- src/views/cadastrofuncionario.py
+++ src/views/cadastrofuncionario.py
@@ -6,14 +6,16 @@ def iniciar():
     cpf = str(formulario.lineEdit_Cpf.text())
     nContato = str(formulario.lineEdit_NumeroContato.text())
     cargo = str(formulario.lineEdit_Cargo.text())
-    dataAdmissao = str(formulario.lineEdit_Data_Admissao.text())
+    dataAdmissao = str(formulario.lineEdit_Data.text())
     pix = str(formulario.lineEdit_Pix.text())
+
+    print(nome, cpf, nContato, cargo, dataAdmissao, pix)
 
 
 
 
 app = QtWidgets.QApplication([])
-formulario = uic.loadUi('cadastro.ui')
+formulario = uic.loadUi("C:/Users/devse/OneDrive/Documentos/GitHub/SISTEMA-DE-GERENCIAMENTO/src/views/cadastro.ui")
 
 formulario.pushButton_Cadastrar.clicked.connect(iniciar)
 

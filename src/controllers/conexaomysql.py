832b8a8a754c81b441f7c5d6d6cd4e7114a770b0
Marcos Serra <marcosserra133@gmail.com>
2023-06-03 23:37:23 -0300

    update

--- src/controllers/conexaomysql.py
+++ src/controllers/conexaomysql.py
@@ -1,2 +1,8 @@
 import mysql.connector
 
+conexao = mysql.connector.connect(
+    host = '127.0.0.1',
+    user = 'root',
+    password = '',
+    database = 'zeus'
+)
--- src/views/cadastrofuncionario.py
+++ src/views/cadastrofuncionario.py
@@ -1,6 +1,14 @@
 from PyQt5 import uic, QtWidgets
+import mysql.connector
 
-def iniciar():
+conexao = mysql.connector.connect(
+    host = '127.0.0.1',
+    user = 'root',
+    password = '',
+    database = 'zeus'
+)
+
+def inserir():
 
     nome = str(formulario.lineEdit_Nome.text())
     cpf = str(formulario.lineEdit_Cpf.text())
@@ -9,7 +17,11 @@ def iniciar():
     dataAdmissao = str(formulario.lineEdit_Data.text())
     pix = str(formulario.lineEdit_Pix.text())
 
-    print(nome, cpf, nContato, cargo, dataAdmissao, pix)
+    cursor = conexao.cursor()
+    comando_SQL = 'INSERT INTO funcionarios (nome, cpf, n_contato, cargo, data_admissao, pix) VALUES (%s, %s, %s, %s, %s, %s)'
+    dados = (nome, cpf, nContato, cargo, dataAdmissao, pix)
+    cursor.execute(comando_SQL, dados)
+    conexao.commit()
 
 
 
@@ -17,7 +29,7 @@ def iniciar():
 app = QtWidgets.QApplication([])
 formulario = uic.loadUi("C:/Users/devse/OneDrive/Documentos/GitHub/SISTEMA-DE-GERENCIAMENTO/src/views/cadastro.ui")
 
-formulario.pushButton_Cadastrar.clicked.connect(iniciar)
+formulario.pushButton_Cadastrar.clicked.connect(inserir)
 
 formulario.show()
 app.exec()
